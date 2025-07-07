73471e96bf2f972eb50b8878992ff8dc2bee4abe
navitat <anlon636@gmail.com>
2025-07-07 15:07:32 +0200

    chores: first commit, cleaned App.jsx, completed iteration 2

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <h1>LAB React Components</h1>
+      <Header />
+    </>
+  )
+}
+
+export default App
